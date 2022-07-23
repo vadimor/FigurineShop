4e2d5a2ea23b6edb6ecb4a9b465a2b86721598fe
vadimor <vadimorbilyu@gmail.com>
2022-07-24 01:40:35 +0300

    Added Services

---
+++ Comment/Services/CommentService.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using Comment.Data;
+using Comment.Data.Entities;
+using Comment.Models.Dtos;
+using Comment.Models.Request;
+using Comment.Repositories.Interfaces;
+using Comment.Services.Interfaces;
+using Infrastructure.Services;
+using Infrastructure.Services.Interfaces;
+
+namespace Comment.Services
+{
+    public class CommentService : BaseDataService<ApplicationDbContext>, ICommentService
+    {
+        private readonly IMapper _mapper;
+        private readonly ICommentRepository _repository;
+        public CommentService(
+            IMapper mapper,
+            ICommentRepository repository,
+            IDbContextWrapper<ApplicationDbContext> dbContextWrapper,
+            ILogger<BaseDataService<ApplicationDbContext>> logger)
+            : base(dbContextWrapper, logger)
+        {
+            _mapper = mapper;
+            _repository = repository;
+        }
+
+        public async Task<CommentDto?> AddCommentAsync(AddRequest request, int userId)
+        {
+            var entity = _mapper.Map<CommentEntity>(request);
+            entity.UserId = userId;
+            var result = await ExecuteSafeAsync(async () =>
+            {
+                return await _repository.AddCommentAsync(entity);
+            });
+
+            return _mapper.Map<CommentDto>(result);
+        }
+
+        public async Task<IEnumerable<CommentDto>> GetCommentsByProductIdAsync(int id)
+        {
+            var entitys = await _repository.GetCommentsByProductIdAsync(id);
+
+            return entitys.Select(x => _mapper.Map<CommentDto>(x));
+        }
+
+        public async Task<CommentDto?> RemoveCommentAsync(int commentId, int userId)
+        {
+            var comment = await _repository.GetCommentAsync(commentId);
+
+            if (comment!.UserId != userId)
+            {
+                return null;
+            }
+
+            var entity = await ExecuteSafeAsync(async () =>
+            {
+                return await _repository.RemoveCommentAsync(commentId);
+            });
+
+            return _mapper.Map<CommentDto>(entity);
+        }
+    }
+}
---
+++ Comment/Services/Interfaces/ICommentService.cs
@@ -0,0 +1,12 @@
+﻿using Comment.Models.Dtos;
+using Comment.Models.Request;
+
+namespace Comment.Services.Interfaces
+{
+    public interface ICommentService
+    {
+        Task<CommentDto?> AddCommentAsync(AddRequest request, int userId);
+        Task<IEnumerable<CommentDto>> GetCommentsByProductIdAsync(int id);
+        Task<CommentDto?> RemoveCommentAsync(int commentId, int userId);
+    }
+}
