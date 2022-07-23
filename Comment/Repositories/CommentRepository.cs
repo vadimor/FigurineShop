ea2a8f81a0f089e686c1e100e2c5b9e5b5f19303
vadimor <vadimorbilyu@gmail.com>
2022-07-24 01:36:24 +0300

    Added Repositories

---
+++ Comment/Repositories/CommentRepository.cs
@@ -0,0 +1,63 @@
+﻿using Comment.Data;
+using Comment.Data.Entities;
+using Comment.Repositories.Interfaces;
+using Infrastructure.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Comment.Repositories
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly ILogger<CommentRepository> _logger;
+
+        public CommentRepository(
+            IDbContextWrapper<ApplicationDbContext> dbContextWrapper,
+            ILogger<CommentRepository> logger)
+        {
+            _dbContext = dbContextWrapper.DbContext;
+            _logger = logger;
+        }
+
+        public async Task<CommentEntity?> GetCommentAsync(int id)
+        {
+            return await _dbContext.Comments.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<IEnumerable<CommentEntity>> GetCommentsByProductIdAsync(int id)
+        {
+            return await _dbContext.Comments.Where(x => x.ProductId == id).ToListAsync();
+        }
+
+        public async Task<CommentEntity?> AddCommentAsync(CommentEntity comment)
+        {
+            var userHaveComment = await _dbContext.Comments
+                .Where(x => x.ProductId == comment.ProductId)
+                .FirstOrDefaultAsync(x => x.UserId == comment.UserId);
+
+            if (userHaveComment is not null)
+            {
+                return null;
+            }
+
+            var entity = await _dbContext.AddAsync(comment);
+            _dbContext.SaveChanges();
+
+            return entity.Entity;
+        }
+
+        public async Task<CommentEntity?> RemoveCommentAsync(int id)
+        {
+            var entity = await _dbContext.Comments.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (entity is not null)
+            {
+                _dbContext.Comments.Remove(entity);
+                _dbContext.SaveChanges();
+                return entity;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Comment/Repositories/Interfaces/ICommentRepository.cs
@@ -0,0 +1,12 @@
+﻿using Comment.Data.Entities;
+
+namespace Comment.Repositories.Interfaces
+{
+    public interface ICommentRepository
+    {
+        Task<CommentEntity?> AddCommentAsync(CommentEntity comment);
+        Task<CommentEntity?> GetCommentAsync(int id);
+        Task<IEnumerable<CommentEntity>> GetCommentsByProductIdAsync(int id);
+        Task<CommentEntity?> RemoveCommentAsync(int id);
+    }
+}
