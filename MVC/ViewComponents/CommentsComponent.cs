34127139b9905747ac84660c4c73c27bdda3030c
vadimor <vadimorbilyu@gmail.com>
2022-07-24 01:55:44 +0300

    Added ViewComponents

---
+++ MVC/ViewComponents/CommentsComponent.cs
@@ -0,0 +1,24 @@
+﻿using MVC.Models.Requests;
+using MVC.Services.Interfaces;
+using MVC.ViewModels.Components;
+
+namespace MVC.ViewComponents
+{
+    public class CommentsComponent : ViewComponent
+    {
+        private readonly ICommentService _commentService;
+
+        public CommentsComponent(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var comments = await _commentService.GetComments(
+                new GetRequest { Id = id });
+
+            return View(new CommentsComponentViewModel { ProductId = id, Comments = comments });
+        }
+    }
+}
