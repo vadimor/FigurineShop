78b3cd167c73e6ee443b61bb517ebb7902981749
vadimor <vadimorbilyu@gmail.com>
2022-07-24 01:40:50 +0300

    Added Mapping

---
+++ Comment/Mapping/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Comment.Data.Entities;
+using Comment.Models.Dtos;
+using Comment.Models.Request;
+
+namespace Comment.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CommentEntity, CommentDto>();
+            CreateMap<AddRequest, CommentEntity>();
+        }
+    }
+}
